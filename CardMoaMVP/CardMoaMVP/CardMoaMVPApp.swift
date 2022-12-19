fb2cf56dc8a8a71a11dc4802922c43e4dcf916b8
l1004ga <l1004ga@gmail.com>
2022-12-19 17:48:33 +0900

    first commit

---
+++ CardMoaMVP/CardMoaMVP/CardMoaMVPApp.swift
@@ -0,0 +1,31 @@
+//
+//  CardMoaMVPApp.swift
+//  CardMoaMVP
+//
+//  Created by dev on 2022/12/19.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct CardMoaMVPApp: App {
+    
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
