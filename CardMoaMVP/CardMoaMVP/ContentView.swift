48d9cd73b5b9bf708be452eeb14c141696223271
jeonghoonji <wlwjdgns0920@naver.com>
2022-12-20 21:22:38 +0900

    12월20일 작업내용 합치기

--- CardMoaMVP/CardMoaMVP/ContentView.swift
+++ CardMoaMVP/CardMoaMVP/ContentView.swift
@@ -8,22 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(Color.white)
+    }
     @EnvironmentObject var authModel : AuthViewModel
-    
     var body: some View {
-        VStack {
-            if authModel.currentUser == nil{
-                LoginView()
-            }else{
-                TestView()
+        if authModel.currentUser == nil{
+            LoginView()
+        }else{
+            TabView {
+                MyCardView().tabItem {
+                    Image(systemName: "creditcard")
+                    Text("나의 카드")
+                }.tag(1)
+                Text("test").tabItem {
+                    Image(systemName: "map")
+                    Text("내 주변 혜택")
+                }.tag(2)
+                BenefitSearchView().tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("혜택 검색")
+                }.tag(3)
+                Text("test").tabItem {
+                    Image(systemName: "list.dash.header.rectangle")
+                    Text("카드 추천")
+                }.tag(4)
+                Text("test").tabItem {
+                    Image(systemName: "person.crop.rectangle")
+                    Text("나의 소비")
+                }.tag(5)
+                TestView().tabItem {
+                    Image(systemName: "person.crop.rectangle")
+                    Text("backTest")
+                }.tag(6)
             }
-
+            .onAppear() {
+                UITabBar.appearance().barTintColor = UIColor(Color.white)
+            }
         }
+        
     }
     
     
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environmentObject(AuthViewModel())
