9495510e1b6ef72fac6a83874808c2f05ca535be
jaehee6 <0jujh8@gmail.com>
2022-12-20 17:33:20 +0900

    BenefitSearch 생성

--- CardMoaMVP/CardMoaMVP/ContentView.swift
+++ CardMoaMVP/CardMoaMVP/ContentView.swift
@@ -8,31 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(Color.white)
+    }
+    
     var body: some View {
-        NavigationStack {
-            TabView {
-                MyCardView().tabItem {
-                    Image(systemName: "creditcard")
-                    Text("나의 카드")
-                }.tag(1)
-                Text("test").tabItem {
-                    Image(systemName: "map")
-                    Text("내 주변 혜택")
-                }.tag(2)
-                
-                Text("test").tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("혜택 검색")
-                }.tag(3)
-                Text("test").tabItem {
-                    Image(systemName: "list.dash.header.rectangle")
-                    Text("카드 추천")
-                }.tag(4)
-                Text("test").tabItem {
-                    Image(systemName: "person.crop.rectangle")
-                    Text("나의 소비")
-                }.tag(5)
-            }
+        //        NavigationStack {
+        TabView {
+            MyCardView().tabItem {
+                Image(systemName: "creditcard")
+                Text("나의 카드")
+            }.tag(1)
+            Text("test").tabItem {
+                Image(systemName: "map")
+                Text("내 주변 혜택")
+            }.tag(2)
+            
+            BenefitSearchView().tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("혜택 검색")
+            }.tag(3)
+            Text("test").tabItem {
+                Image(systemName: "list.dash.header.rectangle")
+                Text("카드 추천")
+            }.tag(4)
+            Text("test").tabItem {
+                Image(systemName: "person.crop.rectangle")
+                Text("나의 소비")
+            }.tag(5)
+            //            }
+        }
+        .onAppear() {
+            UITabBar.appearance().barTintColor = UIColor(Color.white)
         }
         
     }
