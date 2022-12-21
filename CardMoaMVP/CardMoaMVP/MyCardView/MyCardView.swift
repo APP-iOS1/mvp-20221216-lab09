e2489c031e84113fffa79a1335203bd77784d51b
jeonghoonji <wlwjdgns0920@naver.com>
2022-12-21 17:53:30 +0900

    main merge

--- CardMoaMVP/CardMoaMVP/MyCardView/MyCardView.swift
+++ CardMoaMVP/CardMoaMVP/MyCardView/MyCardView.swift
@@ -63,7 +63,7 @@ struct MyCardView: View {
     }
     
 }
-
+//test
 struct MyCardView_Previews: PreviewProvider {
     static var previews: some View {
         MyCardView().environmentObject(ViewModel())
